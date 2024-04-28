01d40466dbf437225cb7128d5a463412ed33f6c4
fahadkabali <kabalifahad@gmail.com>
2024-04-28 23:40:44 +0300

    google auth

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './App/Screens/LoginScreen/Login';
+// import * as dotenv from 'react-native-dotenv';
+import { ClerkProvider,SignedIn, SignedOut } from '@clerk/clerk-expo';
 
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export default function App() {
+  const expoKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
   return (
-    <View style={styles.container}>
-      <Login/>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey={expoKey}
+    >
+      <View style={styles.container}>
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <Login/>
+        </SignedOut>
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider> 
   );
 }
 
--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,22 +1,46 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import * as WebBrowser from "expo-web-browser";
 import Colors from '../../Utils/Colors'
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser";
+
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
+
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
   return (
     <View style={{alignItems:'center'}}>
-      <Image source={require('../assets/logo.png')} 
+      <Image source={require('../../../assets/icon.png')} 
         style={styles.loginImage}
         />
         <View style={styles.subContainer}>
             <Text style={{fontSize:27, color:Colors.WHITE, textAlign:'center'}}>
                 Let's come 
-                <Text style={{fontWeight:'bold'}}>together and talk business</Text> for the services offered.
+                <Text style={{fontWeight:'bold'}}> together and talk business</Text> for the services offered.
             </Text>
             <Text style={{fontSize:17, color:Colors.WHITE, textAlign:'center', marginTop:20}}> 
                 Best of the best at finding the busines for a service needed at an affordable price.
             </Text>
-            <TouchableOpacity style={styles.button} onPress={() => console.log("button clicked!")}>
+            <TouchableOpacity style={styles.button} onPress={onPress}>
                 <Text style={{textAlign:'center', fontSize:17 , color:Colors.PRIMARY}}>Let's Get Started</Text>
             </TouchableOpacity>
         </View>
@@ -27,7 +51,7 @@ export default function Login() {
 const styles = StyleSheet.create({
     loginImage:{
         width:230,
-        height:450,
+        height:250,
         marginTop:100,
         borderWidth:4,
         borderColor:Colors.BLACK,
@@ -40,6 +64,7 @@ const styles = StyleSheet.create({
         borderTopRightRadius:30,
         padding:20,
         marginTop:-20,
+        
     },
     button:{
         padding:15,
